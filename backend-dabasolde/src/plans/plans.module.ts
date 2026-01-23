1ea94bc399f1fcb9e011fc24a6f5c115e142085f
Youssef Abayda <youssefabadya207@gmail.com>
2026-01-23 22:45:57 +0100

    Fix Home page responsive

--- backend-dabasolde/src/plans/plans.module.ts
+++ backend-dabasolde/src/plans/plans.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PlansController } from './plans.controller';
 import { PlansService } from './plans.service';
-// No need to import PrismaModule here because we made it @Global() in Step 0
-
 @Module({
   controllers: [PlansController],
   providers: [PlansService],
